2377aa05374fddd8e58b41ff737bc7da5f93eecf
riklus <lussana.riccardo@gmail.com>
2022-03-04 15:31:56 +0100

    test

--- scripts/bin/scripts.dart
+++ scripts/bin/scripts.dart
@@ -2,7 +2,7 @@ import 'requests.dart';
 
 void main(List<String> arguments) async {
   var s = Requests();
-  var r = await s.get("https://pentoleprofessionali.it");
-  print(s.cookieJar["opera4u.operaunitn.cloud"]);
+  var r = await s.get("https://www.microsoft.com/");
+  print(await r.text());
   return;
 }
